6ff3eccb8653ef17dcee5e6acfbdb72fc362d773
WhiteDark <48309426+pysashapy@users.noreply.github.com>
2020-07-22 14:02:10 +0300

    Add 3.0

--- run.py
+++ run.py
@@ -1,57 +1,49 @@
-import requests
 import csv
-import xlwt
-from bs4 import BeautifulSoup
 import json
 class Pr(object):
     """docstring for property."""
     def __init__(self):
-        self.run()
+        pass
     def getJson(self):
         a = open("input.json","r",encoding="utf-8")
         a = a.read()
         json_string = json.loads(a)
         print(json_string["items"][0])
-        url = "http://www.absolut-tds.com/catalog/gsmrepit/%s/"
-        a = {i["id"]:{"url":url%i["id"],"name":i["name"],"id":i["id"],"price":i["price"],"available":i["available"]} for i in json_string["items"]}
+        url = "http://www.absolut-tds.com/search/?q=%s"
+        a = {i["barcode"]:{"url":url%i["barcode"],"name":i["name"],"id":i["id"],"barcode":i["barcode"],"price":i["price"],"real_q":i["real_q"]} for i in json_string["items"]}
+
         return a
-    def search_get(self,out):
-        a = {}
-        for row in out:
-            try:
-                a[row["Артикул"].split("-")[-1]] =["http://opt-list.ru/admin/store_goods_edit/{0}/all_goods".format(row["Идентификатор товара в магазине"]),row["Цена продажи, без учёта скидок"],row["Остаток"],row["Артикул"]]
-            except Exception as e:
-                print(e)
-        return a
+    def wr(self,data=["JSON Остаток","JSON BarCode","JSON Цена","Название товара у орбиты","Ссылка от орбиты"],l=9):
+        for i,x in zip(range(l,len(data)+l),data): self.row.insert(i, x)
+
     def run(self):
-
         self.__PRODUCT__ = self.getJson()
+        with open('input.csv','r') as csvinput:
+            with open('updata.csv', 'w',newline='') as csvoutput:
+                new = csv.writer(csvoutput, delimiter=';')
+                updata = csv.writer(open('new.csv', 'w',newline=''), delimiter=';')
+                reader = csv.reader(csvinput, delimiter=';')
 
-        out = csv.DictReader(open("input.csv","r"), delimiter=';')
-        get_opt_list = self.search_get(out)
-        book = xlwt.Workbook(encoding="utf-8")
-        sheet1 = book.add_sheet("Python Sheet 1")
-        z = 0
-        print(len(self.__PRODUCT__),list(self.__PRODUCT__.keys()))
-        for i in self.__PRODUCT__.keys():
-            sheet1.write(z, 0, z)
-            sheet1.write(z, 1,  self.__PRODUCT__[i]["url"])
-            sheet1.write(z, 2, self.__PRODUCT__[i]["name"])
-            sheet1.write(z, 3, self.__PRODUCT__[i]["id"])
-            sheet1.write(z, 4, self.__PRODUCT__[i]["price"])
-            sheet1.write(z, 5,  self.__PRODUCT__[i]["available"])
-            try:
-                if get_opt_list[i]:
-                    sheet1.write(z,6, "")
-                    sheet1.write(z,7, get_opt_list[i][0])
-                    sheet1.write(z,8, get_opt_list[i][1])
-                    sheet1.write(z,9, get_opt_list[i][2])
-                    sheet1.write(z,10,get_opt_list[i][3])
-            except Exception as e:
-                pass
-            z+=1
-        book.save("print.xls")
+                newList = []
+                updataList = []
+                self.row = next(reader)
+                self.wr()
+                newList.append(self.row)
 
+                reader1 = {i[4]:i for i in reader}
+                for i in self.__PRODUCT__.keys():
+                    a = self.__PRODUCT__[i]
+                    try:
+                        print(reader1.keys())
+                        self.row = reader1[i]
+                        self.wr([a["real_q"],i,a["price"],a["name"],a["url"]])
+                        newList.append(self.row)
+                    except Exception as e:
+                        updataList.append([a["real_q"],i,a["price"],a["name"],a["url"]])
+
+                new.writerows(newList)
+                updata.writerows(updataList)
 if __name__ == '__main__':
 
     a = Pr()
+    a.run()
